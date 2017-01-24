26012cc310785406f79101209785749b18c58029
Raja Yerram <raja.yerram@siu.edu>
2017-01-24 13:35:01 -0600

    Arrange Commit

--- JavaAWSGITHUB/src/com/prox/training/Arrange.java
+++ JavaAWSGITHUB/src/com/prox/training/Arrange.java
@@ -4,34 +4,17 @@ public class Arrange {
 
 	public static void main(String[] args) {
 		// Assignment 1
-		for(int i=0;i<=5;i++)
+		String s = " ";
+		for(int i=1;i<=5;i++)
 		{
-			if(i==1)
-			{
-				System.out.println("1");
-			}
-			else if(i==2)
-			{
-				System.out.println("1 2");
-				
-			}
-			else if(i==3)
-			{
-				System.out.println("1 2 3");
-			}
-			else if(i==4)
-			{
-				System.out.println("1 2 3 4");
-			}
-			else if(i==5)
-			{
-				System.out.println("1 2 3 4 5");
-			}
+			s = s + i + " ";
+			System.out.println(s);
+			
+			
 		}
-
 	}
-
 }
+
 
 		
 		
