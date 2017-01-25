92798f661b9d377c95edd70fbd41142d65f2b22b
Raja Yerram <raja.yerram@siu.edu>
2017-01-24 21:53:05 -0600

    Assignment 2 Commit

--- JavaAWSGITHUB/src/com/prox/training/Arrange1.java
+++ JavaAWSGITHUB/src/com/prox/training/Arrange1.java
@@ -4,11 +4,8 @@ public class Arrange1 {
 
 	public static void main(String[] args) {
 		// Assignment 2
-		//String s = " ";
 		for(int i=1;i<=5;i++)
 		{
-			//s = s + i + " ";
-			//System.out.println(s);
 			for(int j=1;j<=i;j++)
 			{
 				System.out.print(j + " ");
@@ -17,8 +14,6 @@ public class Arrange1 {
 		}
 		for(int i=4;i>=1;i--)
 		{
-			//s = s + i + " ";
-			//System.out.println(s);
 			for(int j=i;j>0;j--)
 			{
 				System.out.print(j + " ");
