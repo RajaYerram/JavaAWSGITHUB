472d8ef9ffb615a1ccbc653dc0ba45db8c8ec78f
Raja Yerram <raja.yerram@siu.edu>
2017-01-24 22:10:53 -0600

    Assignment 2-1

---
+++ JavaAWSGITHUB/src/com/prox/training/Arrange2.java
@@ -0,0 +1,26 @@
+package com.prox.training;
+
+public class Arrange2 {
+
+		public static void main(String[] args) {
+		// Assignment 2-1
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(i + " ");
+			}
+				System.out.println(" ");
+		}
+		for(int i=4;i>=1;i--)
+		{
+			for(int j=i;j>0;j--)
+			{
+				System.out.print(i + " ");
+			}
+			System.out.println(" ");
+		}
+					
+	}
+}
+
