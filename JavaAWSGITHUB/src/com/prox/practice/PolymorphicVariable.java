26e4593e653c29c5a4b88c7f0f629b7571c1f08f
Raja Yerram <raja.yerram@siu.edu>
2017-01-28 02:28:09 -0600

    Poly and Random

---
+++ JavaAWSGITHUB/src/com/prox/practice/PolymorphicVariable.java
@@ -0,0 +1,9 @@
+package com.prox.practice;
+
+public class PolymorphicVariable {
+	
+	public void show(){
+		System.out.println("This is Polymorphic class");
+	}
+
+}
---
+++ JavaAWSGITHUB/src/com/prox/practice/RandomNumbers.java
@@ -0,0 +1,16 @@
+package com.prox.practice;
+
+import java.util.Random;
+
+public class RandomNumbers {
+
+		public static void main(String[] args) {
+			
+			Random random = new Random();
+			for(int i=0; i<10; i++)
+			{
+				System.out.println(random.nextInt(10));
+			}
+			
+		}
+}
