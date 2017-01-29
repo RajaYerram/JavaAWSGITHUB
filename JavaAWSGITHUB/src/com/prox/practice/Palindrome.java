a2ed9dbff0f84ba7aa065fa468fd34f46210de74
Raja Yerram <raja.yerram@siu.edu>
2017-01-28 20:52:08 -0600

    Palindrome

---
+++ JavaAWSGITHUB/src/com/prox/practice/Palindrome.java
@@ -0,0 +1,30 @@
+package com.prox.practice;
+
+import java.util.Scanner;
+
+public class Palindrome {
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		long num = s.nextLong();
+		String strorg = "" + num;
+		String strrev = "";
+		int len = strorg.length();
+		for(int i = len-1;i>=0;i--)
+		{
+			strrev = strrev + strorg.charAt(i);
+		}
+		if(strorg.equals(strrev)){
+			System.out.println("The number is a palindrome");
+		}
+		else{
+			System.out.println("The number is not a palindrome");
+		}
+		s.close();
+		
+	}
+
+}
+
+
